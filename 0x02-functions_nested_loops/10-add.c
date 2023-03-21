d45bf14ca6df8faa3640416d0a81822b35db0c85
Orlando-Gama <orlando03gama@gmail.com>
2023-03-21 12:38:30 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * add - add two numbers
+  * @b: the first number
+  * @c: the second number
+  * Return: the addition of both
+  */
+int add(int b, int c)
+{
+	int sum;
+
+	sum = b + c;
+
+	return (sum);
+}
+
