73ea4430dc4c54d1cedd90ec8968748d22d7e7e9
Orlando-Gama <orlando03gama@gmail.com>
2023-03-23 05:19:21 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-	int i; /*declare an integer variable*/
+	int i;
 
 	printf("infinite loop incoming :(\n");
 
+	i = 0;
 	/**
-	  * i = 0;
-	  * while (i < 10)
-	  *{
-	  *	putchar(i);
-	  *}
-	  */
+	*while (i < 10)
+	*{
+		putchar(i);
+	*}
+	*/
 	printf("infinite loop avoided! \\0/\n");
 
 	return (0);
