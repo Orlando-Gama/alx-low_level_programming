3f9297117b4f961614a2f60dc284a7bc0d60c938
Orlando-Gama <orlando03gama@gmail.com>
2023-03-24 07:12:20 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int p, z;
+
+		for (p = 0; p < size; p++)
+		{
+			for (z = 0; z < size; z++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
