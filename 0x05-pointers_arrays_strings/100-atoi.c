7a65a2a441e07c7ea76be6e2db37824751e7bb14
Orlando-Gama <orlando03gama@gmail.com>
2023-03-27 06:40:48 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,11 @@
 */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int q, d, g, len, f, digit;
 
-	i = 0;
+	q = 0;
 	d = 0;
-	n = 0;
+	g = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
@@ -21,31 +21,31 @@ int _atoi(char *s)
 
 		len++;
 
-	while (i < len && f == 0)
+	while (q < len && f == 0)
 	{
-		if (s[i] == '-')
+		if (s[q] == '-')
 			++d;
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[q] >= '0' && s[q] <= '9')
 		{
-			digit = s[i] - '0';
+			digit = s[q] - '0';
 
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digit;
+			g = g * 10 + digit;
 
 			f = 1;
 
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[q + 1] < '0' || s[q + 1] > '9')
 				break;
 			f = 0;
 		}
 
-			i++;
+			q++;
 	}
 	if (f == 0)
 
 		return (0);
 
-	return (n);
+	return (g);
 }
 
