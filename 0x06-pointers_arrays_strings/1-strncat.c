e8acbf4249ef0cc70d7b5f063a71bfb101f53edf
Orlando-Gama <orlando03gama@gmail.com>
2023-03-29 12:27:31 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,6 +8,7 @@
   * @n: input value
   * Return: dest
   */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int a;
@@ -26,6 +27,7 @@ char *_strncat(char *dest, char *src, int n)
 		a++;
 		b++;
 	}
+
 	dest[a] = '\0';
 	return (dest);
 }
