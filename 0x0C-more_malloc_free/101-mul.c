28f285838b830cb6ce24601f1b21d272ac9ce42d
Orlando-Gama <orlando03gama@gmail.com>
2023-04-13 15:26:41 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * main - multiplies two positive numbers
-  * @argc: n arguments
-  * @argv: args
-  * Return: int
-  */
-int main(int argc, char *argv[])
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
 {
-	unsigned long mul;
-	int i, j;
+	int m = 0;
 
-	if (argc != 3)
+	while (s[m])
 	{
-		printf("Error\n");
-		exit(98);
+		if (s[m] < '0' || s[m] > '9')
+			return (0);
+		m++;
 	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+	return (1);
 }
