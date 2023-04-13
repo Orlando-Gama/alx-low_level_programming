2c81ca7a223d18fa618544bcd67b34bfbe78e2d2
Orlando-Gama <orlando03gama@gmail.com>
2023-04-13 06:08:06 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,28 +11,28 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int q = 0, x = 0, pen1 = 0, pen2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
+	while (s1 && s1[pen1])
+		pen1++;
+	while (s2 && s2[pen2])
+		pen2++;
+	if (n < pen2)
+		s = malloc(sizeof(char) * (pen1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (pen1 + pen2 + 1));
 	if (!s)
 		return (NULL);
-	while (i < len1)
+	while (q < pen1)
 	{
-		s[i] = s1[i];
-		i++;
+		s[q] = s1[q];
+		q++;
 	}
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-	s[i] = '\0';
+	while (n < pen2 && q < (pen1 + n))
+		s[q++] = s2[x++];
+	while (n >= pen2 && q < (pen1 + pen2))
+		s[q++] = s2[x++];
+	s[q] = '\0';
 	return (s);
 }
 
