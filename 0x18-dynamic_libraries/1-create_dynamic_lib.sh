86fee72c02a390811d7ec1936257ba4c15b09ae1
Orlando-Gama <orlando03gama@gmail.com>
2023-08-25 14:08:48 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-ar -rc liball.a *.o
+
+# Compile all .c files into position-independent object files (-fPIC)
+gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c
+
+# Create the dynamic library 'liball.so' from the object files
+gcc -shared -o liball.so *.o
+
+# Clean up by removing the object files
+rm *.o
+
