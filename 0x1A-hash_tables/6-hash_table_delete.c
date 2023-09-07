8ce7300d9f898b45bc9f887f223d3adf97c901a1
Orlando-Gama <orlando03gama@gmail.com>
2023-09-07 14:35:37 -0700

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - this deletes a hash table.
+ * @ht: pointer to hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int n;
+
+	for (n = 0; n < ht->size; n++)
+	{
+		if (ht->array[n] != NULL)
+		{
+			node = ht->array[n];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+
+	free(head);
+}
